ccffc87515fbc626fb55458cadf1abcee9d3603d
Gushley110 <gushley110@gmail.com>
2020-05-13 22:52:14 -0500

    Hardcode change

--- mac_changer.py
+++ mac_changer.py
@@ -1,3 +1,5 @@
 import subprocess
 
-subprocess.call('ifconfig', shell=True)
+subprocess.call('ifconfig wlp2s0 down', shell=True)
+subprocess.call('ifconfig wlp2s0 hw ether 00:11:22:33:44:55', shell=True)
+subprocess.call('ifconfig wlp2s0 up', shell=True)
