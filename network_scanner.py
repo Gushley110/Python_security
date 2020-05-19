53f610538c095c5f2293a0adf081de7419ac3003
Gushley110 <gushley110@gmail.com>
2020-05-18 23:40:35 -0500

    Merged ethernet and arp package

--- network_scanner.py
+++ network_scanner.py
@@ -2,7 +2,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst = ip)    
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst = "FF:FF:FF:FF:FF:FF")
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 
 scan('192.168.1.1/24')
